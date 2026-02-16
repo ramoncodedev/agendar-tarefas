9621d2f12b90d4636ab441fcc0e391ef2929b43b
ramonocode <ramonwebdesigner2025@gmail.com>
2026-02-16 09:06:20 -0300

    commit inicial

---
+++ src/main/java/com/agendador/agendadador/de/tarefas/AgendadadorDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.agendador.agendadador.de.tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadadorDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadadorDeTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agendador/agendadador/de/tarefas/AgendadadorDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agendador.agendadador.de.tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadadorDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
